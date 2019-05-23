8d65b121b481d01c79f3e039f74cbbda8430fe7a
spartanwolf141 <49828012+spartanwolf141@users.noreply.github.com>
2019-05-22 21:08:35 -1000

    completed to exercise 2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 /*Hey Prepsters! Let's help Ronald McDonald find true love by revising his online dating profile!*/
 
 //1. In the nav element, target the span element with the id of 'matCount' and change the number from 0 to 11.
-
+var match = document.getElementById("matCount");
+match.innerHTML = 11;
 //2. In the nav element, target the span element with the id of 'msgCount' and change the number from 0 to 23.
-
+var message = document.getElementById("msgCount");
+message.innerHTML = 23; 
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
 
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
